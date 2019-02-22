5203a177c06110482b5a2f9978b47ba17ebc31da
Arne Schacher <Arne.Schacher@deutschebahn.com>
2019-02-22 18:15:42 +0100

    added various website components

---
+++ src/app/components/chatroom-item/chatroom-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatroom-item',
+  templateUrl: './chatroom-item.component.html',
+  styleUrls: ['./chatroom-item.component.scss']
+})
+export class ChatroomItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
