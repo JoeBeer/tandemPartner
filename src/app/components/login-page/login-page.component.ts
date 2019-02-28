509c466591273d21141147c145b55f98db2fe202
Arne Schacher <Arne.Schacher@deutschebahn.com>
2019-02-28 21:32:42 +0100

    added login functionality

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authService: AuthService) {
+                this.loginForm = this.createLoginForm();
+               }
 
   ngOnInit() {
   }
 
+  createLoginForm() {
+    return this.formBuilder.group({
+      loginFormMail: ['', Validators.required],
+      loginFormPassword: ['', Validators.required]
+    });
+  }
+
+  login() {
+    const mail = this.loginForm.value.loginFormMail;
+    const password = this.loginForm.value.loginFormPassword;
+
+    this.authService.signInWithMailAndPassword(mail, password);
+  }
+
+  get loginFormMail() {
+    return this.loginForm.get('loginFormMail');
+  }
+
+  get loginFormPassword() {
+    return this.loginForm.get('loginFormPassword');
+  }
 }
