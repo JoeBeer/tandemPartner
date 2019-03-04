77dbaf5834edf502cc489e241121a0c41592ad15
Arne Schacher <Arne.Schacher@deutschebahn.com>
2019-03-04 20:41:43 +0100

    edited base structure of home.html & added modal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,6 +5,8 @@ import { UserStoreService } from 'src/app/services/user-store.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -15,8 +17,19 @@ export class HomeComponent implements OnInit {
 
   users: User[];
   user: User;
+  requestUser: User;
   matchRequests: Match[];
   initiatorFirstname: string;
+
+  // for fontawesome icons
+  faCheck = faCheck;
+  faTimes = faTimes;
+
+  // for pagination
+  pageNumber = 1;
+
+  // for modal
+  display = 'none';
 
   constructor(
     private userStoreService: UserStoreService,
@@ -48,13 +61,27 @@ export class HomeComponent implements OnInit {
   }
 
   acceptMatch() {
-    // TODO: add functionality
+    // TODO: add functionality - update the match
     console.log('accepted');
+  //  this.showMatchRequestsForCurrentUser(this.authService.currentUser.uid);
   }
 
-  deleteMatch() {
-    // TODO: add delete functionality
+  declineMatch() {
+    // TODO: add delete functionality - delete the match
     console.log('deleted');
+   // this.showMatchRequestsForCurrentUser(this.authService.currentUser.uid);
+  }
+
+  openModal(id: string) {
+    console.log('id: ' + id);
+    this.display = 'block';
+    this.userStoreService.getUserById(id).subscribe((recievedUser: User) => {
+      this.requestUser = recievedUser;
+    });
+  }
+
+  closeModal() {
+    this.display = 'none';
   }
 
 }
