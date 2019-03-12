d85ab19610f8ac7d011273022a81d0e866f3e99e
Luis Ramer <luis-raphael.ramer@deutschebahn.com>
2019-03-12 15:02:42 +0100

    changed createUser from Post to Put method

--- functions/index.js
+++ functions/index.js
@@ -22,17 +22,16 @@ let chatroomsEndpoint = require('./chatroomsEndpoint')
 
 
 users.get('/', usersEndpoint.getUsers); //Get all users
-users.get('/:user-id', usersEndpoint.getUserById);// Get one user
-users.post('/', usersEndpoint.createUser); // Create new user
-users.put('/:user-id', usersEndpoint.updateUser); // Update user
-users.delete('/:user-id', usersEndpoint.deleteUser); // Delete a user
-users.get('/:user-id/matches', usersEndpoint.getMatches); // Gets the matches from a user
+users.get('/:userId', usersEndpoint.getUserById);// Get one user
+users.put('/:userId', usersEndpoint.createUser); // Create new user
+//users.put('/:user-id', usersEndpoint.updateUser); // Update user
+users.delete('/:userId', usersEndpoint.deleteUser); // Delete a user
 exports.users = functions.https.onRequest(users); //Enables function 'users' @ Cloud Functions
 
 matches.post('/', matchesEndpoint.createMatch); //Create new match
-matches.get('/:match-id', matchesEndpoint.getMatch); //Get one match
+matches.get('/:matchId', matchesEndpoint.getMatch); //Get one match
 exports.matches = functions.https.onRequest(matches); //Enables function 'matches' @ Cloud Functions
 
 chatrooms.post('/', chatroomsEndpoint.createChatroom); // Create new chatroom
-chatrooms.get('/:chatroom-id', chatroomsEndpoint.getChatroomById) // Get one chatroom
+chatrooms.get('/:chatroomId', chatroomsEndpoint.getChatroomById) // Get one chatroom
 exports.chatrooms = functions.https.onRequest(chatrooms); //Enables function 'chatrooms' @ Cloud Functions
