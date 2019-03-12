326ca407d49e80dca68d9b58163babeb6d2477c9
Arne Schacher <Arne.Schacher@deutschebahn.com>
2019-03-12 16:36:58 +0100

    changed authService for publishing the currentUser

--- angular/src/app/models/user.ts
+++ angular/src/app/models/user.ts
@@ -1,12 +1,12 @@
 export class User {
   constructor(
+    public uid: string,
     public firstname: string,
     public lastname: string,
-    public city: string,
     public dateOfBirth: Date,
+    public sex: string,
+    public city: string,
     public activities: string[],
     public offers: string[],
-    public sex: string,
-    public mail?: string
-  ) {}
+    ) {}
 }
