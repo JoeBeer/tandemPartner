62962e0bb94d6bad9b76072d848fd3ecf9ee934a
Eric Odschuk <Eric.Odschuk@deutschebahn.com>
2019-03-25 12:06:41 +0100

    edit password validators

--- angular/src/app/components/register-page/register-page.component.ts
+++ angular/src/app/components/register-page/register-page.component.ts
@@ -64,11 +64,11 @@ export class RegisterPageComponent implements OnInit {
 
       // at least 6 characters, must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number, can contain special characters
       // tslint:disable-next-line:max-line-length
-      registerFormPassword: ['', [Validators.required, /*Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$'),*/ Validators.minLength(6), Validators.maxLength(16)]],
+      registerFormPassword: ['', [Validators.required, Validators.pattern('^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d).{6,}$')/* , Validators.minLength(6), Validators.maxLength(16) */]],
 
       // at least 6 characters, must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number, can contain special characters
       // tslint:disable-next-line:max-line-length
-      registerFormPasswordConfirm: ['', [Validators.required, /*Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$'),*/ Validators.minLength(6), Validators.maxLength(16)]]
+      registerFormPasswordConfirm: ['', [Validators.required, Validators.pattern('^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d).{6,}$')/* , Validators.minLength(6), Validators.maxLength(16) */]]
         // adds the custom validator for validating the passwords og their matching
     }, { validator: this.passwordMatchValidator});
   }
