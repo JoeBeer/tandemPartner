25a2432818ecb7f408ea53f2c84d319c42c60dec
Arne Schacher <Arne.Schacher@deutschebahn.com>
2019-04-09 14:56:25 +0200

    configured the project's apiUrls & environment for the tandemFirebase-project

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyA60SCLQd6f7KOT_UmUml5RgMnAC3F5E_A',
-    projectId: 'livechattandem'
+    apiKey: 'AIzaSyCww8YKdmbCbawimU1yEzpNDb6jHpr8fzs',
+    projectId: 'tandemfirebase'
   }
 };
